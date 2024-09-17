77e4243c9f6909a40adaaab2a8466a22bb0f2b4c
Juna1d1 <kucinggot215@gmail.com>
2024-09-17 10:20:27 +0700

    Update Latihan.js

--- Latihan.js
+++ Latihan.js
@@ -1,1 +1,31 @@
+// Mendefinisikan Kelas
+class Mobil {
+    constructor(merk, warna) {
+        this.merk = merk;
+        this.warna = warna;
+    }
 
+    // Metode
+    jalan() {
+        console.log(`${this.merk} berwarna ${this.warna} sedang melaju di jalan.`);
+    }
+}
+
+// Pewarisan
+class MobilBalap extends Mobil {
+    constructor(merk, warna, kecepatanMaksimal) {
+        super(merk, warna); // Memanggil konstruktor kelas induk
+        this.kecepatanMaksimal = kecepatanMaksimal;
+    }
+
+    // Overriding Metode
+    jalan() {
+        console.log(`${this.merk} berwarna ${this.warna} dengan kecepatan maksimal ${this.kecepatanMaksimal} km/jam.`);
+    }
+}
+
+// Membuat Objek
+const mobil1 = new Mobil('SkyLine', 'Biru Putih');
+mobil1.jalan();
+const mobilBalap1 = new MobilBalap('Supra MK5', 'Hitam', 500);
+mobilBalap1.jalan();
